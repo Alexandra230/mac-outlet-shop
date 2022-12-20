118fef95c162cd8e6516e96acf81cda837ffef1f
Olexandra <sasha5l239@gmail.com>
2022-12-20 17:38:32 +0200

    test netlify

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ app.use(express.static(__dirname));
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
-app.listen(process.env.PORT || port, function () {
+app.listen(process.env.NETLIFY || port, function () {
   console.log(`Server listens http://${host}:${port}`);
 });
 
