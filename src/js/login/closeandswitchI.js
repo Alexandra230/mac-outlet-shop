1e800591936e767c545907b973a9b74a06842f0b
Olexandra <sasha5l239@gmail.com>
2023-02-09 12:58:01 +0200

    delete close btn

--- src/js/login/closeandswitchI.js
+++ src/js/login/closeandswitchI.js
@@ -1,18 +1,18 @@
-let close = document.getElementById('exit-login');
+// let close = document.getElementById('exit-login');
 let signupForm = document.getElementById('form-container-signup');
 let toSignupBtn = document.getElementById('to-signup-form');
 let loginForm = document.getElementById('form-container-login');
 let shop = document.getElementById('wrapper');
 
-close.addEventListener('click', function (e) {
-  e.stopPropagation();
-  if (loginForm.style.display === 'block') {
-    loginForm.style.display = 'none';
-  } else {
-    loginForm.style.display = 'block';
-  }
-  shop.style.display = 'block';
-});
+// close.addEventListener('click', function (e) {
+//   e.stopPropagation();
+//   if (loginForm.style.display === 'block') {
+//     loginForm.style.display = 'none';
+//   } else {
+//     loginForm.style.display = 'block';
+//   }
+//   shop.style.display = 'block';
+// });
 
 toSignupBtn.addEventListener('click', function (e) {
   e.preventDefault();
--- src/js/signup/closeandswitchS.js
+++ src/js/signup/closeandswitchS.js
@@ -1,18 +1,18 @@
-let close = document.getElementById('exit-signup');
+// let close = document.getElementById('exit-signup');
 let signupForm = document.getElementById('form-container-signup');
 let toLoginBtn = document.getElementById('to-login-form');
 let loginForm = document.getElementById('form-container-login');
 let shop = document.getElementById('wrapper');
 
-close.addEventListener('click', function (e) {
-  e.stopPropagation();
-  if (signupForm.style.display === 'block') {
-    signupForm.style.display = 'none';
-  } else {
-    signupForm.style.display = 'block';
-  }
-  shop.style.display = 'block';
-});
+// close.addEventListener('click', function (e) {
+//   e.stopPropagation();
+//   if (signupForm.style.display === 'block') {
+//     signupForm.style.display = 'none';
+//   } else {
+//     signupForm.style.display = 'block';
+//   }
+//   shop.style.display = 'block';
+// });
 
 toLoginBtn.addEventListener('click', function (e) {
   e.preventDefault();
